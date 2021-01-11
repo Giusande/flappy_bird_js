48df747b3f18fa185edd35b69a66ef3506bb6372
iamageo <geovani.af4@gmail.com>
2021-01-11 10:50:53 -0300

    draw bird

--- jogo.js
+++ jogo.js
@@ -1,5 +1,23 @@
+console.log("Flappy JS")
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+
+function loop() {
+    
+    contexto.drawImage(
+        sprites,
+        0, 0, //sprite x, sprite y
+        33, 24, // tmanho do recorte na sprite
+        10, 50,
+        33, 24, //tamanho do desenho
+    );
+
+    requestAnimationFrame(loop);
+}
+
+loop();
