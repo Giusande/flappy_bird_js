427f5b9b36be742f3f702bf2b4bb0a6139d7df48
iamageo <geovani.af4@gmail.com>
2021-01-12 10:39:42 -0300

    create gravity

--- jogo.js
+++ jogo.js
@@ -67,7 +67,6 @@ const chao = {
     }
 }
 
-
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
@@ -75,7 +74,12 @@ const flappyBird = {
     altura: 24,
     x: 10,
     y: 50,
-
+    gravidade: 0.25,
+    velocidade: 0,
+    atualiza () {
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
+        flappyBird.y = flappyBird.y + flappyBird.velocidade
+    },
     desenha() {
         contexto.drawImage(
             sprites,
@@ -89,15 +93,13 @@ const flappyBird = {
 }
 
 
-
 function loop() {
+    flappyBird.atualiza()
     
     planoDeFundo.desenha();
-
     chao.desenha();
-    
     flappyBird.desenha();
-   
+    
     requestAnimationFrame(loop);
 }
 
